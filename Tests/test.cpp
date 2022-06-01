01348f55fcfa5a85070c68dea54c806e1e509328
bartekf4 <56221703+bartekf4@users.noreply.github.com>
2022-06-01 17:09:48 +0200

    add GUI

---
+++ Tests/test.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    char str[30];
+     cout<<"Enter Your Name ";
+     cin>>str;
+    std::cout<<"Hello "<<str<<"!";
+     cout<<endl;
+     return 0;
+
+ }
---
+++ Tests/test.py
@@ -0,0 +1,15 @@
+# int main()
+# {
+#     char str[30];
+#     cout<<"Enter Your Name ";
+#     cin>>str;
+#     std::cout<<"Hello "<<str<<"!";
+#     cout<<endl;
+#     return 0;
+#
+# }
+
+if __name__ == '__main__':
+    str0 = input("Enter Your Name ")
+    print("Hello ", str0, "!")
+    input("Press Enter to continue...")
