69a6322a127df407ceb1cf150f05a6dd154be963
bartekf4 <56221703+bartekf4@users.noreply.github.com>
2022-06-20 21:14:00 +0200

    tests

---
+++ antlr_approach/FileErrorListener.py
@@ -0,0 +1,36 @@
+from antlr4.error.ErrorListener import ErrorListener
+import pprint
+
+
+class FileErrorListener(ErrorListener):
+    __path = "dist/diag.txt"
+    __errors = ""
+
+    def __init__(self):
+        super().__init__()
+        open(self.__path, 'w').close()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.__errors += "line " + str(line) + ":" + str(column) + " " + msg + "\n"
+        self.write()
+
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        # self.__errors += "reportAmbiguity " + str(ambigAlts) + ":" + str(
+        #     configs) + ", input=" + self.parser.getTokenStream().getText(interval)
+        pass
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        # self.__errors += "reportAttemptingFullContext decision=" + str(dfa.decision) + ":" + str(
+        #     configs) + ", input=" + self.parser.getTokenStream().getText(interval)
+        pass
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        # self.__error += "reportContextSensitivity decision=" + str(dfa.decision) + ":" + str(
+        #     configs) + ", input=" + self.parser.getTokenStream().getText(interval)
+        pass
+
+    def write(self):
+        with open(self.__path, 'w') as file:
+            file.writelines(self.__errors)
+
